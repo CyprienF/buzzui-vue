dfc690259f1b1e9ab45347b09bd1c48d94d1c3bd
CyprienF <floquet.cyprien@gmail.com>
2022-11-11 12:25:19 +0100

    feat(BuzzAvatar): add new component

---
+++ src/components/BuzzAvatar.vue
@@ -0,0 +1,40 @@
+<template>
+  <img class="avatar" :src="image" :class="computedClasses" />
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+import { Shape } from '../types/shape';
+import { config } from '../utils/config';
+
+interface Props {
+  image: string;
+  shape?: Shape;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  image: config.defaultAvatarImage as string,
+  shape: config.defaultShape as Shape,
+});
+
+const image = computed(() => props.image);
+const computedClasses = computed(() => {
+  return {
+    avatar__rounded: props.shape === 'rounded',
+  };
+});
+</script>
+
+<style scoped lang="scss">
+.avatar {
+  width: 4rem;
+  height: 4rem;
+
+  border-radius: 0.3rem;
+
+  &__rounded {
+    border-radius: 20rem;
+  }
+}
+</style>
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,4 +4,5 @@ export const config = {
   defaultShape: 'squared',
   defaultDisabled: false,
   defaultOutlined: false,
+  defaultAvatarImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn35yS42ahMEoiyqgI_BmTXHzTv0hBWM1x0Q&usqp=CAU',
 };
